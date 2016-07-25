4474c1f7c5f4f64ad178a14c6ec6857ec3e72d01
eduardolevygp <eduardo.levygp@gmail.com>
2016-07-24 23:19:10 -0300

    criação do projeto em symfony

---
+++ Backend/ace_backend/src/AceBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AceBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('AceBundle:Default:index.html.twig');
+    }
+}
