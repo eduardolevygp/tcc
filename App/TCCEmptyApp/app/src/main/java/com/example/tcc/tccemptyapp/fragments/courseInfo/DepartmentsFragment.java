33fec44bbf13f1da474623d98e3dc4ac278c05fe
alanraso <alanrraso@gmail.com>
2016-10-23 23:04:18 -0200

    Estrutura de orgnização dos cursos em singleton separado

--- App/TCCEmptyApp/app/src/main/java/com/example/tcc/tccemptyapp/fragments/courseInfo/DepartmentsFragment.java
+++ App/TCCEmptyApp/app/src/main/java/com/example/tcc/tccemptyapp/fragments/courseInfo/DepartmentsFragment.java
@@ -7,6 +7,7 @@ import com.example.tcc.tccemptyapp.adapters.courseInfo.DepartmentsListener;
 import com.example.tcc.tccemptyapp.fragments.BaseFragment;
 import com.example.tcc.tccemptyapp.helpers.TransactionHelper;
 import com.example.tcc.tccemptyapp.models.courseInfo.Course;
+import com.example.tcc.tccemptyapp.models.courseInfo.CourseInfoStructure;
 import com.example.tcc.tccemptyapp.models.courseInfo.Department;
 import com.example.tcc.tccemptyapp.models.courseInfo.Period;
 import com.example.tcc.tccemptyapp.models.courseInfo.Program;
@@ -30,7 +31,7 @@ public class DepartmentsFragment extends CourseInfoFragment {
 
     @Override
     protected CourseInfoAdapter getAdapter() {
-        return new DepartmentsAdapter(getContext(), getMockDepartments(), getDepartmentsListener());
+        return new DepartmentsAdapter(getContext(), CourseInfoStructure.getInstance().getDepartmentList(), getDepartmentsListener());
     }
 
     private DepartmentsListener getDepartmentsListener() {
@@ -52,61 +53,4 @@ public class DepartmentsFragment extends CourseInfoFragment {
         };
     }
 
-    private List<Department> getMockDepartments() {
-        Department dp1 = new Department();
-        dp1.setName("PSI");
-        dp1.setPeriodList(getMockPSIPeriods());
-
-        Department dp2 = new Department();
-        dp2.setName("PCS");
-        dp2.setProgramList(getMockPCSPrograms());
-
-        List<Department> departments = new ArrayList<>();
-        departments.add(dp1);
-        departments.add(dp2);
-
-        return departments;
-    }
-
-    private List<Period> getMockPSIPeriods() {
-        Period period1 = new Period();
-        Period period2 = new Period();
-        List<Period> periods = new ArrayList<>();
-
-        period1.setName("Primeiro Semestre");
-        period1.setId(1);
-        period2.setName("Segundo Semestre");
-        period2.setId(2);
-        periods.addAll(Arrays.asList(period1, period2));
-
-        return periods;
-    }
-
-    private List<Program> getMockPCSPrograms() {
-        Program program1 = new Program();
-        Program program2 = new Program();
-        List<Program> programs = new ArrayList<>();
-
-        program1.setName("Computação Cooperativo");
-        program1.setPeriodList(getCoopMockPeriodList());
-        program2.setName("Computação Semestral");
-        program2.setId(5);
-        programs.addAll(Arrays.asList(program1, program2));
-
-        return programs;
-    }
-
-    private List<Period> getCoopMockPeriodList() {
-        Period period1 = new Period();
-        Period period2 = new Period();
-        List<Period> periods = new ArrayList<>();
-
-        period1.setName("Quinto Quadrimestre");
-        period1.setId(3);
-        period2.setName("Sexto Quadrimestre");
-        period2.setId(4);
-        periods.addAll(Arrays.asList(period1, period2));
-
-        return periods;
-    }
 }
